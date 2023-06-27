b245afafba8d1e5828d7b2b150e969cac63dc437
LeeKyuSung <ks5050577@gmail.com>
2023-06-27 23:43:02 +0900

    저장 데이터 표시 기능 추가

--- src/main/resources/static/js/location.js
+++ src/main/resources/static/js/location.js
@@ -1,6 +1,7 @@
 const locationText = document.querySelector(".location");
 const cityText = document.querySelector(".city");
 const saveButton = document.querySelector(".save-button");
+const locationList = document.querySelector(".location-list");
 
 let lat, lng;
 let cityName;
@@ -46,8 +47,39 @@ function onSaveButtonClick() {
     .then((data) => {
       console.log(data);
       alert("위치 정보가 저장되었습니다.");
+      refreshLocationList();
+    });
+}
+
+function refreshLocationList() {
+  fetch("/location")
+    .then((response) => response.json())
+    .then((locations) => {
+      console.log(locations);
+      locationList.innerHTML = "";
+
+      locationList.innerHTML = `
+        <tr>
+          <th>이름</th>
+          <th>도시</th>
+          <th>위도</th>
+          <th>경도</th>
+          <th>저장 시간</th>
+        </tr>`;
+      locations.forEach((location) => {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+          <td>${location.name}</td>
+          <td>${location.cityName}</td>
+          <td>${location.lat}</td>
+          <td>${location.lng}</td>
+          <td>${location.createdDate}</td>
+        `;
+        locationList.appendChild(tr);
+      });
     });
 }
 
 navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
 saveButton.addEventListener("click", onSaveButtonClick);
+refreshLocationList();
