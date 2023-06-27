230e46471301e722029e11f96d132465fdde3c16
LeeKyuSung <ks5050577@gmail.com>
2023-06-27 11:11:58 +0900

    Initial commit

---
+++ src/main/java/kr/kyusung/locationsaver/LocationSaverApplication.java
@@ -0,0 +1,13 @@
+package kr.kyusung.locationsaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationSaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationSaverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/kyusung/locationsaver/LocationSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kyusung.locationsaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationSaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
